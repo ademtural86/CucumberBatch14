a842ada826c8d84835817f46587383bf17e01fd2
ademtural86 <ademtural86@gmail.com>
2022-12-04 21:31:29 +0100

    04-12-2022

---
+++ src/test/java/steps/DashboardSteps.java
@@ -0,0 +1,16 @@
+package steps;
+
+import io.cucumber.java.en.Then;
+
+public class DashboardSteps {
+
+    @Then("user verify dashboard page")
+    public void user_verify_dashboard_page() {
+<<<<<<< HEAD
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+=======
+        System.out.println("Batch 14 is happy now");
+>>>>>>> afcfc4246523e3fd6d182033b4ec10e2fd56828b
+    }
+}
